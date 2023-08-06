53f1ba657be3adb5bea27adbd84760d9fb6a2025
lapolazzati <lapo.lazzati@gmail.com>
2023-08-06 12:57:52 -0300

    removed twitter

--- ice_breaker.py
+++ ice_breaker.py
@@ -3,9 +3,7 @@ from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
-from agents.twitter_lookup_agent import lookup as twitter_lookup_agent
 from third_parties.linkedin import scrape_linkedin_profile
-from third_parties.twitter import scrape_user_tweets
 import output_parsers
 
 
@@ -13,9 +11,7 @@ def ice_breaker(name: str) -> str:
     linkedin_profile_url = linkedin_lookup_agent(name=name)
     linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_profile_url)
 
-    # twitter_username = twitter_lookup_agent(name=name)
-    # tweets = scrape_user_tweets(username=twitter_username, num_tweets=5)
-
+   
     summary_template = """
          given the Linkedin information {linkedin_information} and about a person from I want you to create:
          1. a short summary
